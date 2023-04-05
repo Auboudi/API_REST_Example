8adb5851304b80acb33dc080f5fa149c2b245719
aboudinot <aurore.boudinot174@gmail.com>
2023-04-05 11:10:32 +0200

    Capas de User creadas

---
+++ src/main/java/com/example/user/User.java
@@ -0,0 +1,40 @@
+package com.example.user;
+
+import org.hibernate.annotations.NaturalId;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "_users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+
+   @Column(unique = true) // No se repite el valor, pero no forma parte de la PK
+   // @NaturalId(mutable = true) Para que forme parte de la PK pero si se puede repetir
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role Role;
+}
